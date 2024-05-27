9d5cea255480c8fe636bdafdc1d12a97cb8bbdfd
unknown <isaevallahverdi@gmail.com>
2024-05-27 14:29:55 +0400

    done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,14 +7,19 @@ const Navbar = () => {
             <div className="logo">
                 <img src="/src/assets/img/logo.png" alt="logo" />
             </div>
+
             <div className="navigation">
                 <ul>
-                    <li><a href="">What is Chaplean</a> </li>
-                    <li><a href="">Our difference</a></li>
-                    <li><a href="">Pricing</a></li>
-                    <li><a href="">Docs</a></li>
-                    <li><a href=""><span>Contact Us</span></a></li>
+                    <li><a href="#">What is Chaplean</a> </li>
+                    <li><a href="#">Our difference</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Docs</a></li>
+                    <li><a href="#"><span>Contact Us</span></a></li>
                 </ul>
+            </div>
+
+            <div className="hamburger">
+                <img src="/src/assets/svg/open.png" alt="open" />
             </div>
         </div>
     )
